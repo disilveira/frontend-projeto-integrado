0228a9ee42858d35ce5670d46020ff6cdc17b7d7
Diego Almeida <diego.almeida@outlook.com>
2021-07-01 23:21:33 -0300

    Rotas criadas

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+const routes = [
+  { path: '/login', component: Login},
+  { path: '/home', component: Home}
+]
+
+const router = new VueRouter({
+  routes
+})
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
