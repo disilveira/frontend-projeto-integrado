69e22cf04449c658d280f82440b4caa0cbdb317a
Diego Almeida <diego.almeida@outlook.com>
2021-07-23 10:20:10 -0300

    Configurando arquivos para deploy no heroku

---
+++ dist/js/chunk-2d0d328a.97e8c580.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d328a"],{"5c28":function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},c=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"home"},[t("h1",[e._v("Home")]),t("h3",[e._v("Dashboard Inicial Aqui")])])}],s={name:"Home"},i=s,l=t("2877"),r=Object(l["a"])(i,a,c,!1,null,null,null);n["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d0d328a.97e8c580.js.map
