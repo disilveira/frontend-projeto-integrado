70247a11ada27c226c1e9751e9172e549ac6858e
Diego Almeida <diego.almeida@outlook.com>
2021-07-27 09:53:18 -0300

    Listagem de usuários implementada na rot

--- src/pages/users/Users.vue
+++ src/pages/users/Users.vue
@@ -1,18 +1,65 @@
 <template>
   <div id="users">
-    <h1>Usuários</h1>
+    <div class="container-scroller">
+      <div class="container-fluid page-body-wrapper">
+        <NavBarComponent :user_name="user.user_name"/>
+        <SideBarComponent />
+        <div class="main-panel">
+          <div class="content-wrapper">
+            <div class="row">
+              <div class="col-lg-12 grid-margin stretch-card">
+                <div class="card">
+                  <div class="card-body">
+                    <h4 class="card-title">Lista de Usuários</h4>
+                    <div class="table-responsive">
+                      <table class="table table-striped">
+                        <thead>
+                          <tr>
+                            <th>#</th>
+                            <th>Nome</th>
+                            <th>E-mail</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr :key="user.user_id" v-for="user in users">
+                            <td>{{ user.user_id }}</td>
+                            <td>{{ user.name }}</td>
+                            <td>{{ user.email }}</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions, mapState } from 'vuex'
+import NavBarComponent from '../../components/Layout/NavBarComponent.vue'
+import SideBarComponent from '../../components/Layout/SideBarComponent.vue'
 
 export default {
+  name: 'Users',
   mounted () {
     this.ActionListUsers()
   },
+  computed: {
+    ...mapState('users', ['users']),
+    ...mapState('auth', ['user'])
+  },
   methods: {
     ...mapActions('users', ['ActionListUsers'])
+  },
+  components: {
+    NavBarComponent,
+    SideBarComponent
   }
 }
 </script>
