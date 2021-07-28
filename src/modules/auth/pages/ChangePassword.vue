38b50a3f38f7fb6f632d108fa7dc39fd3d6b4761
Diego Almeida <diego.almeida@outlook.com>
2021-07-28 09:13:56 -0300

    Página para alteração de senha configurada

---
+++ src/modules/auth/pages/ChangePassword.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+      <div class="content-wrapper d-flex align-items-stretch auth auth-img-bg">
+        <div class="row flex-grow">
+          <div
+            class="col-lg-6 d-flex align-items-center justify-content-center"
+          >
+            <div class="auth-form-transparent text-left p-3">
+              <div class="brand-logo">
+                <h1>SS Telemática</h1>
+              </div>
+              <h6 class="font-weight-light">
+                Dashboard de Estatísticas de Frota
+              </h6>
+              <form @submit.prevent="submit()" class="pt-3">
+                <div class="form-group">
+                  <label for="exampleInputEmail">E-mail</label>
+                  <div class="input-group">
+                    <div class="input-group-prepend bg-transparent">
+                      <span
+                        class="input-group-text bg-transparent border-right-0"
+                      >
+                        <i class="ti-user text-primary"></i>
+                      </span>
+                    </div>
+                    <input
+                      type="email"
+                      class="form-control form-control-lg border-left-0"
+                      id="exampleInputEmail"
+                      placeholder="Email"
+                      v-model="email"
+                      required
+                    />
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail">Nova Senha</label>
+                  <div class="input-group">
+                    <div class="input-group-prepend bg-transparent">
+                      <span
+                        class="input-group-text bg-transparent border-right-0"
+                      >
+                        <i class="ti-lock text-primary"></i>
+                      </span>
+                    </div>
+                    <input
+                      type="password"
+                      class="form-control form-control-lg border-left-0"
+                      id="exampleInputEmail"
+                      placeholder="Nova Senha"
+                      v-model="password"
+                      required
+                    />
+                  </div>
+                </div>
+                <div
+                  class="my-2 d-flex justify-content-between align-items-center"
+                >
+                  <router-link :to="{ name: 'Home' }"
+                        >Ir para o login</router-link
+                      >
+                </div>
+                <div class="my-3">
+                  <button
+                    class="
+                      btn btn-block btn-primary btn-lg
+                      font-weight-medium
+                      auth-form-btn
+                    "
+                  >
+                    ALTERAR SENHA
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div class="col-lg-6 login-half-bg d-flex flex-row">
+            <p
+              class="
+                text-white
+                font-weight-medium
+                text-center
+                flex-grow
+                align-self-end
+              "
+            >
+              Copyright &copy; 2021 Todos os direitos reservados.
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- content-wrapper ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    submit () {
+      try {
+        const postData = { email: this.email }
+        this.$http.post('https://api-projeto-integrado.herokuapp.com/users/forgotPassword', postData).then(res => {
+          alert(res.body.message)
+          this.$router.push({ name: 'Login' })
+        })
+      } catch (err) {
+        alert(err.data ? err.data.message : 'Não foi possível efetuar o login')
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/modules/auth/routes.js
+++ src/modules/auth/routes.js
@@ -8,5 +8,10 @@ export default [
     path: '/request-password',
     name: 'RequestPassword',
     component: () => import(/* webpackChunkName: "RequestPassword" */ './pages/RequestPassword')
+  },
+  {
+    path: '/change-password',
+    name: 'ChangePassword',
+    component: () => import(/* webpackChunkName: "ChangePassword" */ './pages/ChangePassword')
   }
 ]
